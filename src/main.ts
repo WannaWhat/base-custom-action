b6a01195e14e654b62e6ddca1c113e55096d2745
artem_DESCTOP <artemyi03@tutanota.com>
2021-09-05 22:47:59 +0300

    Go to typescript

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import * as core from '@actions/core';
+import {isString} from "util";
 const YAML = require('yaml')
 const fs = require('fs')
 
@@ -18,33 +19,39 @@ function main(): void {
     try {
     console.log('Start deployment script');
     const CONFIG_FILE: any = get_config();
-        if (BRANCH in CONFIG_FILE['dep_branches']) {
-        } else {
-            throw new Error(`Current branch: ${BRANCH} not in config file: ${CONFIG_PATH}`);
-        }
-        let BRANCH_FILE: any = CONFIG_FILE['dep_branches'][BRANCH];
-        if ('env' in BRANCH_FILE){
-            let env_path: string = BRANCH_FILE['env']['path']
-            let file = fs.readFileSync(env_path, 'utf8')
-            file += '\n'
-            let enviroments: any = BRANCH_FILE['env']['append'];
-            for (let key in enviroments){
-                if (enviroments[key]['secret']){
-                    file += `${key}=${process.env[key]}\n`
-                } else {
-                    file += `${key}=${enviroments[key]['name']}\n`
+    console.log(CONFIG_FILE['dep_branches']['master']['env']['append'])
+    if (BRANCH in CONFIG_FILE['dep_branches']) {
+    } else {
+        throw new Error(`Current branch: ${BRANCH} not in config file: ${CONFIG_PATH}`);
+    }
+    let BRANCH_FILE: any = CONFIG_FILE['dep_branches'][BRANCH];
+    if ('env' in BRANCH_FILE){
+        let env_path: string = BRANCH_FILE['env']['path']
+        let file = fs.readFileSync(env_path, 'utf8')
+        file += '\n'
+        let enviroments: any = BRANCH_FILE['env']['append'];
+        for (let key in enviroments){
+            let key_name: string = ''
+            if (isString(enviroments[key])) {
+                key_name = enviroments[key]
+                file += `${key_name}=${process.env[key_name]}\n`
+            } else {
+                for (let index_key in enviroments[key]) {
+                    key_name = index_key
                 }
+                file += `${key_name}=${enviroments[key][key_name]}\n`
             }
-            fs.writeFileSync(env_path, file)
-            console.log(file)
         }
+        fs.writeFileSync(env_path, file)
+        console.log(file)
+    }
     }
     catch (error) {
         console.error(error.message);
     }
 }
 // Initial github inputs
-const BRANCH = core.getInput('branch')
-// const BRANCH: string = 'master'
+// const BRANCH = core.getInput('branch')
+const BRANCH: string = 'master'
 
 main()
