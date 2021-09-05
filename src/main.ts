3d9299d2c65bb749d78011ac9dcc12560111c1b5
artem_DESCTOP <artemyi03@tutanota.com>
2021-09-05 20:13:46 +0300

    Go to typescript

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,33 @@
-import * as core from '@actions/core';
+// import * as core from '@actions/core';
+//
+//
+// async function run() {
+//     try {
+//         const BRANCH = core.getInput('branch')
+//         console.log('Current branch', BRANCH)
+//     }
+//     catch (error) {
+//         core.setFailed(error.message)
+//     }
+// }
+//
+//
+//
+//
+// run()
+const YAML = require('yaml')
+const fs = require('fs')
 
+function parse_config(): void {
+    const file = fs.readFileSync('test.yml', 'utf8')
+    let a: any = YAML.parse(file)
+    console.log(a)
+}
 
-async function run() {
-    try {
-        const BRANCH = core.getInput('branch')
-        console.log('Current branch', BRANCH)
-    }
-    catch (error) {
-        core.setFailed(error.message)
-    }
+function main(): void {
+    console.log('Start deployment script')
+    parse_config()
 }
 
 
-
-
-console.log('Start deployment script')
-run()
+main()
