097d48b95da3055eed35c5b954d8f2f22584b5a5
artem_DESCTOP <artemyi03@tutanota.com>
2021-09-05 18:13:52 +0300

    Go to typescript

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,18 @@
-console.log('alpha bera gammea')
+import * as core from '@actions/core';
+
+
+async function run() {
+    try {
+        const BRANCH = core.getInput('branch')
+        console.log('Current branch', BRANCH)
+    }
+    catch (error) {
+        core.setFailed(error.message)
+    }
+}
+
+
+
+
+console.log('Start deployment script')
+run()
